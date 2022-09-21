ba860b95cec50ee6f926bb0ed84a92ae4066a5c5
Valentin <valentin.rudloff.perso@gmail.com>
2022-09-22 00:54:09 +0200

    make the docker compose work :)

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python project/manage.py makemigrations raw2dng;
+python project/manage.py createsuperuser --noinput
+python project/manage.py makemigrations raw2dng  --noinput;
 python project/manage.py migrate raw2dng --noinput;
 python project/manage.py migrate --noinput;
 python project/manage.py runserver 0.0.0.0:8000;
